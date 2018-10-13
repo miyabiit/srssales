8e2a0ddbbc626e338717b1e0a7b2b70558dae6e3
baker@BALLOONGA <yamada.masahiro@gmail.com>
2018-10-13 09:40:56 +0900

    ユーザ管理画面、ビジュアルエディターなどを見せなくする

--- functions.php
+++ functions.php
@@ -6,6 +6,13 @@ function remove_menus(){
 }
 add_action('admin_menu', 'remove_menus');
 
+//ユーザプロフィール項目の追加
+function my_user_meta($wb){
+  $wb['office_code'] = '所属営業所コード';
+  return $wb;
+}
+add_filter('user_contactmethods', 'my_user_meta', 10, 1);
+
 // タイトルのプレースフォルダを変更
 add_filter( 'enter_title_here', 'custom_enter_title_here', 10, 2 );
 function custom_enter_title_here( $enter_title_here, $post ) {
@@ -49,3 +56,29 @@ require_once locate_template('include/wpcf7-tag-post.php', true);
 require_once locate_template('include/google-map-code.php', true);
 //その他funcions
 require_once locate_template('include/myfunctions.php', true);
+
+function profile_delete() { ?>
+    <script type="text/javascript">
+        tftn = "table.form-table:nth-of-type";
+        jQuery(document).ready(function() {
+            jQuery("div#profile-page h3").css("display", "none");//『個人設定』『名前』『連絡先情報』の大タイトルを非表示に
+            jQuery(tftn + "(1) tr:nth-child(1)").css("display", "none");//『ビジュアルエディター』を非表示に
+            jQuery(tftn + "(1) tr:nth-child(2)").css("display", "none");//『管理画面の配色』を非表示に
+            jQuery(tftn + "(1) tr:nth-child(3)").css("display", "none");//『キーボードショートカット』を非表示に
+            jQuery(tftn + "(1) tr:nth-child(4)").css("display", "none");//『ツールバー』を非表示に
+            jQuery(tftn + "(2) tr:nth-child(1)").css("display", "none");//『ユーザ名』を非表示に
+            //jQuery(tftn + "(2) tr:nth-child(2)").css("display", "none");//『名』を非表示に
+            //jQuery(tftn + "(2) tr:nth-child(3)").css("display", "none");//『姓』を非表示に
+            jQuery(tftn + "(2) tr:nth-child(4)").css("display", "none");//『ニックネーム』を非表示に（※この項目は必須です）
+            jQuery(tftn + "(2) tr:nth-child(5)").css("display", "none");//『ブログ上の表示名』を非表示に
+            //jQuery(tftn + "(3) tr:nth-child(1)").css("display", "none");//『メールアドレス』を非表示に（※この項目は必須です）
+            jQuery(tftn + "(3) tr:nth-child(2)").css("display", "none");//『ウェブサイト』を非表示に
+            //jQuery(tftn + "(4) tr:nth-child(1)").css("display", "none");//『プロフィール情報』を非表示に
+            //jQuery(tftn + "(4) tr:nth-child(2)").css("display", "none");//『新しいパスワード』を非表示に
+            //jQuery(tftn + "(4) tr:nth-child(3)").css("display", "none");//『新しいパスワードを再入力』を非表示に
+         });
+    </script>
+<?php 
+}
+add_action( 'show_user_profile', 'profile_delete' );
+
